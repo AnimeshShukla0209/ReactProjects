a499e47250f81209505704a8e37c3d3a82fbb7fe
Animesh.Shukla2 <2416983@cognizant.com>
2025-07-12 17:32:50 +0530

    Project

---
+++ OneDrive/Desktop/OnlineShopping-main/src/app/home/shopCardData.ts
@@ -0,0 +1,22 @@
+export let shopCards = [
+    {
+        icon: "fa-truck",
+        header: "Fast Delivery"
+    },
+    {
+        icon: "fa-ship",
+        header: "Free Shipping"
+    },
+    {
+        icon: "fa-ranking-star",
+        header: "Best Quality"
+    },
+    {
+        icon: "fa-rotate-left",
+        header: "Easy Returns"
+    },
+    {
+        icon: "fa-lock",
+        header: "Secure Payment"
+    }
+];
